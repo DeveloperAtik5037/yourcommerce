7d5aa5ceafaabd0354c987216f1054f3b4060654
OsysDigital <atik.osysdigital@gmail.com>
2024-01-06 19:12:45 +0600

    add

--- resources/themes/default/web-views/checkout/shipping.blade.php
+++ resources/themes/default/web-views/checkout/shipping.blade.php
@@ -449,7 +449,8 @@
             }
         }
     </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key={{\App\CPU\Helpers::get_business_settings('map_api_key')}}&libraries=places&v=3.52"></script>
+    <script
+        src="https://maps.googleapis.com/maps/api/js?key={{\App\CPU\Helpers::get_business_settings('map_api_key')}}&libraries=places&v=3.49"></script>
     <script>
         function initAutocomplete() {
             var myLatLng = {
