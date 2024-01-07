6d7d45f90f79a73d2c65a4b11ccda4664abf3e47
OsysDigital <atik.osysdigital@gmail.com>
2024-01-07 15:35:44 +0600

    add

--- resources/views/seller-views/product/add-new.blade.php
+++ resources/views/seller-views/product/add-new.blade.php
@@ -60,8 +60,8 @@
                                         <label class="title-color"
                                             for="{{ $lang }}_description">{{ \App\CPU\translate('description') }}
                                             ({{ strtoupper($lang) }})</label>
-                                        {{-- <textarea name="description[]" class="editor textarea" cols="30" rows="10" required>{{ old('details') }}</textarea> --}}
-                                        <textarea name="description[]" class="textarea editor-textarea" id="editor">{{ old('details') }}</textarea>
+                                        <textarea name="description[]" class="editor textarea" id="editor" cols="30" rows="10" required>{{ old('details') }}</textarea>
+                                        {{-- <textarea name="description[]" class="textarea editor-textarea" id="editor">{{ old('details') }}</textarea> --}}
                                     </div>
                                 </div>
                             @endforeach
